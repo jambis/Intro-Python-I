8d420f6ceafe43fe2dcf40186dd8acf4c0c45d0b
jambis <imjaamees@gmail.com>
2020-02-10 16:04:47 -0600

    Completed functions, scopes and file io

--- src/10_functions.py
+++ src/10_functions.py
@@ -7,6 +7,14 @@ num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
 
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+even_or_odd(num)
